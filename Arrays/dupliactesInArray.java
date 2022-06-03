a433034ad15be8b49ba5067aa9eb86a9e9dca4ec
Ishita Amod <68494260+ishita-git@users.noreply.github.com>
2022-06-03 00:12:53 -0700

    Add files via upload

---
+++ Arrays/dupliactesInArray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        Arrays.sort(nums);
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i-1]==nums[i])
+            {
+                list.add(nums[i]);
+            }
+        }
+        return list;
+    }
+}
